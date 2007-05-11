700b183fc3142ab2bdb6bf0726bd2cb0244270b3
Ruslan Zakirov <ruz@bestpractical.com>
2007-05-11 21:39:47 +0000

    add new method into RT::ConditionSLA

--- lib/RT/Condition/SLA.pm
+++ lib/RT/Condition/SLA.pm
@@ -11,4 +11,26 @@ use base qw(RT::Extension::SLA RT::Condition::Generic);
 
 sub SLAIsApplied { return 1 }
 
+=head1 IsCustomFieldChange
+
+=cut
+
+sub IsCustomFieldChange {
+    my $self = shift;
+    my $cf_name = shift;
+
+    my $txn = $self->TransactionObj;
+    
+    return 0 unless $txn->Type eq 'CustomField';
+
+    my $cf = $self->TicketObj->QueueObj->CustomField( $cf_name );
+    unless ( $cf->id ) {
+        $RT::Logger->error("Couldn't load the '$cf_name' field");
+        return 0;
+    }
+
+    return 0 unless $cf->id == $txn->Field;
+    return 1;
+}
+
 1;
