6772e835714c9900a82aae77d9639a4b48a0e31f
Ruslan Zakirov <ruz@bestpractical.com>
2007-05-14 17:35:48 +0000

    implement SetStarts action

--- lib/RT/Action/SLA_SetStarts.pm
+++ lib/RT/Action/SLA_SetStarts.pm
@@ -5,34 +5,35 @@ package RT::Action::SLA_SetStarts;
 
 use base qw(RT::Action::SLA);
 
-=head2 Prepare
+=head1 NAME
 
-Always run this action.
+RT::Action::SLA_SetStarts - set starts date field of a ticket according to SLA
+
+=head1 DESCRIPTION
+
+Look up the SLA of the ticket and set the Starts date accordingly. Nothing happens
+if the ticket has no SLA defined.
+
+Note that this action doesn't check if Starts field is set already, so you can
+use it set the field in a force mode or can protect field using a condition
+that checks value of Starts.
 
 =cut
 
 sub Prepare { return 1 }
 
-=head2 Commit
-
-Look up the SLA and set the Starts date accordingly unless it's allready set.
-
-=cut
-
 sub Commit {
     my $self = shift;
 
     my $ticket = $self->TicketObj;
+    my $level = $ticket->FirstCustomFieldValue('SLA');
+    unless ( $level ) {
+        $RT::Logger->debug('Ticket #'. $ticket->id .' has no dervice level defined, skip setting Starts');
+        return 1;
+    }
 
-    # get out of here if have date set
-    return 1 $ticket->StartsObj->Unix > 0;
-
-    # XXX: we must use SLA to set starts
-    my $bizhours = RT::Estension::SLA::BusinessHours();
-
-    my $starts = $bizhours->first_after(
-        $self->TransactionObj->CreatedObj->Unix
-    );
+    my $SLA = $self->Agreements;
+    my $starts = $SLA->Starts( $self->TransactionObj->CreatedObj->Unix, $level );
 
     my $date = RT::Date->new($RT::SystemUser);
     $date->Set( Format => 'unix', Value => $starts );
